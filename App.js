7297fe14979ba72699fd44254ff0e7d48b5da1b0
Pushpendra Singh <pushpendra.singh@intellectdesign.com>
2018-07-16 19:55:36 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View , ScrollView} from 'react-native';
+import CameraRoll from './camera'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.image}>
+          <Text style={{ textAlign: 'center', margin:10, fontSize:24, color:'#024b7c' }}> Text Extraction  </Text>
+          <ScrollView showsVerticalScrollIndicator={false} >
+            <CameraRoll/>
+          </ScrollView>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ceebff',
+    width: '100%',
+  },
+  image:{
+    marginTop: 20,
+    padding: 15,
+    width: '100%',
+    marginBottom : 30
+  }
+});
---
+++ camera.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Button, Image, View, Text, ScrollView } from 'react-native';
+import { ImagePicker } from 'expo';
+
+
+export default class CameraRoll extends React.Component {
+  state = {
+    image: null,
+    textFound: null
+  };
+
+  render() {    
+
+    let { image } = this.state;
+
+    return (
+      <View >
+          <Button
+            title="Select Image"
+            onPress={this._pickImage}
+          />
+          {image && <Image source={{ uri: image }} style={{ width: '100%', height: 200, marginTop:30 }} />}
+
+          <View style={{ marginTop:30 }}>
+              <Button
+              title="Process"
+              onPress={this._sendImage}            
+              />
+          </View>
+
+          <View style={{ flex:1, padding: 10, marginTop: 20 }}>
+            <Text>{this.state.textFound}</Text>
+          </View>
+      </View>
+    );
+  }
+    _pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            allowsEditing: true
+        });
+        //console.log(result);
+        if (!result.cancelled) {
+        this.setState({ image: result.uri });
+        }
+    };
+
+    _sendImage = async () => {
+
+      const data = new FormData();
+      data.append('img',{
+        uri: this.state.image,
+        type: 'image/jpeg',
+        name: 'testPic'
+      });
+      fetch('https://fbf33287.ngrok.io',{
+        method: 'POST',
+        body: data
+      }).then(response => {
+        return response.json();
+      }).then(data=> {
+        this.setState({textFound : data['Text']});
+        //console.log(data['Text']);
+      }
+      ).catch(console.log);
+    };
+}
